60763064a23dd580c7b158a16b4d00d34b0417e3
brsaca <brsaca@gmail.com>
2023-11-27 16:24:46 -0600

    Initial Commit

---
+++ AIAssistant/AIAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIAssistantApp.swift
+//  AIAssistant
+//
+//  Created by Brenda Saavedra Cantu on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AIAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIAssistant
+//
+//  Created by Brenda Saavedra Cantu on 27/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
