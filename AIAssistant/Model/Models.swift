28032d8c46fcfcd7be0293117ef2d41c5f6b4077
brsaca <brsaca@gmail.com>
2023-11-28 16:51:28 -0600

    feat: add models

---
+++ AIAssistant/Model/Models.swift
@@ -0,0 +1,25 @@
+//
+//  Models.swift
+//  AIAssistant
+//
+//  Created by Brenda Saavedra Cantu on 28/11/23.
+//
+
+import Foundation
+
+enum VoiceType: String, Codable, Hashable, Sendable, CaseIterable {
+    case alloy
+    case echo
+    case fable
+    case onyx
+    case nova
+    case shimmer
+}
+
+enum VoiceChatState {
+    case idle
+    case recordingSpeech
+    case processingSpeech
+    case playingSpeech
+    case error(Error)
+}
