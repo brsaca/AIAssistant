a6654191dfcb80bcbb3b11d6d94d3c19ed186a5f
brsaca <brsaca@gmail.com>
2023-11-29 15:49:06 -0600

    feat: add ViewModel

---
+++ AIAssistant/ViewModel/ViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ViewModel.swift
+//  AIAssistant
+//
+//  Created by Brenda Saavedra Cantu on 28/11/23.
+//
+
+import AVFoundation
+import Foundation
+import Observation
+import XCAOpenAIClient
+
+@Observable
+final class ViewModel: NSObject {
+    
+    let client = OpenAIClient(apiKey: Config.openAIKey)
+    
+    var selectedVoice = VoiceType.alloy
+    var state = VoiceChatState.idle {
+        didSet {
+            print(state)
+        }
+    }
+    var isIdle: Bool {
+        if case .idle = state {
+            return true
+        }
+        return false
+    }
+    var audioPower = 0.0
+}
